9d00fe89c74b9c54f0ab2135796f831f3c9a6c37
Diya Sharma <sharmadiya7200@gmail.com>
2025-03-13 00:43:17 -0700

    add get method for cities and its attractions

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import cors from "cors";
 import "dotenv/config";
 import express from "express";
-import cityRouter from "./routes/cities.js";
+import { cityRouter} from "./routes/cities.js";
+import attractionsRouter from "./routes/attractions.js";
 const app = express();
 
 const PORT = process.env.PORT || 5050;
@@ -11,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/cities", cityRouter);
+app.use("/api/attractions", attractionsRouter);
 
 
 app.listen(PORT, () => {
--- routes/cities.js
+++ routes/cities.js
@@ -1,10 +1,22 @@
 import * as cityController from "../controllers/city-controller.js";
+import * as attractionsController from "../controllers/attractions-controller.js";
 
 import express from "express";
 const cityRouter = express.Router();
+const attractionsRouter = express.Router();
+
 
 cityRouter
     .route("/")
     .get(cityController.getAllCities);
 
-export default cityRouter;    
+cityRouter
+    .route("/:id")
+    .get(cityController.getCityById);
+
+cityRouter
+    .route("/:id/attractions")
+    .get(cityController.getAllAttractionsByCities);
+
+
+export { cityRouter, attractionsRouter};    
