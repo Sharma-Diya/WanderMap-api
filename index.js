d1cbbeed371fb44a0adba0fe08b97fb47d293f5a
Diya Sharma <sharmadiya7200@gmail.com>
2025-03-12 23:29:52 -0700

    set route for cityRouter on port

---
+++ index.js
@@ -0,0 +1,18 @@
+import cors from "cors";
+import "dotenv/config";
+import express from "express";
+import cityRouter from "./routes/cities.js";
+const app = express();
+
+const PORT = process.env.PORT || 5050;
+
+app.use(express.json());
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/cities", cityRouter);
+
+
+app.listen(PORT, () => {
+  console.log(`SERVER STARTED SUCCESSFULLY AND LISTENING AT ${PORT}`);
+});
